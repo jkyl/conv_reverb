79a700efcd55dfea73eed6ebed157fbeaa7cc1cf
Jonathan Kyl <jkyl@Jonathans-MacBook-Air.local>
2016-01-24 16:21:18 -0600

    added plot_fft_spectrum method

--- conv_reverb/audio.py
+++ conv_reverb/audio.py
@@ -12,7 +12,8 @@ class Audio:
         if not fname is None:
             self._segment = utils.read_audio_file_to_obj(fname)
             self._array = utils.audio_obj_to_arrays(self._segment)
-            self._title = fname.split('.')[0]
+            self._mono_array = (self._array[0] + self._array[1])/2.
+            self._title = ''.join(fname.split('/')[1:])
         else:
             self._array = None
             self._title = 'Untitled'
@@ -26,6 +27,10 @@ class Audio:
     def array(self):
         return self._array
     
+    @property
+    def mono_array(self):
+        return self._mono_array
+    
     def set_title(self, title):
         self._title = title
         
@@ -33,26 +38,34 @@ class Audio:
         if array.shape[0] == 2:
             self._array = array
 
-    def write_to_wav(self, fname):
-        utils.write_stereo_arrays_to_wav(self._array, fname)
+    def write_to_wav(self):
+        utils.write_stereo_arrays_to_wav(self._array,
+                                         ''.join(self._title.split('.')[:-1]))
 
-    def plot_waveform(self, output_fname = None):
-        utils.plot_waveform(self._array, self._title, output_fname)
+    def plot_waveform(self):
+        utils.plot_waveform(self._array, ''.join(self._title.split('.')[:-1]))
+
+    def plot_fft_spectrum(self, window_size_in_samples):
+        spectrum = utils.get_fft(self._mono_array, window_size_in_samples)
+        utils.plot_fft(spectrum, ''.join(self._title.split('.')[:-1]))
 
     def convolve(self, audio_obj):
         conv = array_transforms.convolve(self._array, audio_obj.array)
         rv = Audio()
         rv.set_array(conv)
+        rv.set_title(self.title + 'convolved with' + audio_obj.title)
         return rv
         
     def pitchshift(self, amt):
         shift = array_transforms.pitchshift(self._array, amt)
         rv = Audio()
         rv.set_array(shift)
+        rv.set_title(self.title + 'pitch shifted %s %'%(amt * 100))
         return rv
 
     def ringmod(self, freq_hz):
         mod = array_transforms.ringmod(self._array, freq_hz)
         rv = Audio()
         rv.set_array(mod)
+        rv.set_title(self.title + 'ring modulated at %s Hz'%(freq_hz))
         return rv
--- conv_reverb/utils.py
+++ conv_reverb/utils.py
@@ -1,5 +1,7 @@
 from pydub import AudioSegment
 from scipy.io.wavfile import write
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from scipy.interpolate import interp2d
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
@@ -13,7 +15,7 @@ def read_audio_file_to_obj(fname):
     Creates an AudioSegment object of the filename. Converts filetype suffix
     if necessary. 
     '''
-    ftype = fname.split('.')[1]
+    ftype = fname.split('.')[-1]
     if ftype in SUFFIXES:
         ftype = SUFFIXES[ftype]
     return AudioSegment.from_file(fname, ftype)
@@ -30,10 +32,11 @@ def audio_obj_to_arrays(obj):
     return np.array([s.get_array_of_samples() for s in split])
 
 
-def plot_waveform(a, title, output_fname = None):
+def plot_waveform(a, title):
     '''
     '''
     x_in_secs = np.arange(len(a[0]))/44100.
+    plt.close('all')
     fig, (ax1, ax2) = plt.subplots(2, sharex=True, sharey=True)
     fig.subplots_adjust(left=0.05, bottom=0.08, right=0.98, top=0.92)
     plt.suptitle('"'+title+'"', style='italic')
@@ -47,13 +50,11 @@ def plot_waveform(a, title, output_fname = None):
     ax2.axes.get_yaxis().set_visible(False)
     plt.xlabel('Seconds')
     plt.xticks(np.arange(0, round(x_in_secs[-1]), round(x_in_secs[-1]/10.)))
-    plt.show()
-    if not output_fname is None:
-        plt.savefig(output_fname)
+    plt.savefig('output/waveforms/' + title)
     
     
     
-def write_stereo_arrays_to_wav(stereo_array, output_fname):
+def write_stereo_arrays_to_wav(stereo_array, title):
     '''
     Normalizes the convolved waveform and swaps the axes to the way the scipy
     writer likes it. Writes as a .wav to the specified filepath. 
@@ -61,7 +62,51 @@ def write_stereo_arrays_to_wav(stereo_array, output_fname):
     norm = np.int16(32767 * stereo_array/float(np.max(np.abs(stereo_array))))
     if norm.shape[0] == 2:
         norm = norm.swapaxes(0, 1)
-    write(output_fname, 44100, norm)
+    write('output/wavfiles/' + title, 44100, norm)
+
+
+def get_fft(a, step_size):
+    '''
+    '''
+    trunc_a = a[:(len(a) - (len(a) % step_size))]
+    split_into_steps = np.array_split(trunc_a, len(a) / step_size)
+    fourier_coeffs = [np.fft.fft(s) for s in split_into_steps]
+    windows = np.array([[np.abs(coeff) for coeff in window[:len(window)/2]] \
+                        for window in fourier_coeffs])
+    spectrum = windows/windows.max()
+    spectrum[np.where(spectrum < .0000009)] = .0000009  # apply noise floor to 
+    return 10*np.log10(spectrum.swapaxes(0,1))          # -inf points.    
+
+
+
+def plot_fft(spectrum, title):
+    '''
+    '''
+    n_freq_bins, n_time_windows = spectrum.shape
+    x_axis = np.linspace(0, n_freq_bins*n_time_windows/44100., n_time_windows)
+    y_axis = np.linspace(0, 22050, n_freq_bins)
+    interpolation =interp2d(x_axis, y_axis, spectrum, kind = 'cubic')
+    new_x = np.linspace(0, x_axis.max(), 500)
+    new_y = np.linspace(0, y_axis.max(), 5000)
+    spectrum = interpolation(new_x, new_y)
+    spectrum = spectrum - spectrum.max()
+    X, Y = np.meshgrid(new_x, new_y)
+    
+    plt.close('all')
+    ax = plt.gca()
+    im = ax.pcolormesh(X, Y, spectrum, cmap = 'afmhot')
+    plt.title(title)
+    plt.xlim(0, x_axis.max())
+    plt.yscale('log')
+    plt.ylim([22.05, 22050])
+    plt.xlabel('Time (seconds)')
+    plt.ylabel('Frequency (Hz)')
+    plt.tick_params(axis='y', which='minor')
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    plt.colorbar(im, cax=cax, ticks = np.arange(0, -70, -10),
+                 label = 'Amplitude (dB)')
+    plt.savefig('output/spectra/' + title + '.png')
     
     
 
