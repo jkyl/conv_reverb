dafaf70f8428fc9ae3acdef20ce61668f066bb01
Jonathan Kyl <jkyl@Jonathans-Air.att.net>
2016-01-25 00:35:10 -0600

    added examples

--- conv_reverb/audio.py
+++ conv_reverb/audio.py
@@ -12,8 +12,7 @@ class Audio:
         if not fname is None:
             self._segment = utils.read_audio_file_to_obj(fname)
             self._array = utils.audio_obj_to_arrays(self._segment)
-            self._mono_array = (self._array[0] + self._array[1])/2.
-            self._title = ''.join(fname.split('/')[1:])
+            self._title = ''.join(''.join(fname.split('/')[1:]).split('.')[:-1])
         else:
             self._array = None
             self._title = 'Untitled'
@@ -29,7 +28,7 @@ class Audio:
     
     @property
     def mono_array(self):
-        return self._mono_array
+        return (self._array[0] + self._array[1])/2.
     
     def set_title(self, title):
         self._title = title
@@ -39,33 +38,32 @@ class Audio:
             self._array = array
 
     def write_to_wav(self):
-        utils.write_stereo_arrays_to_wav(self._array,
-                                         ''.join(self._title.split('.')[:-1]))
+        utils.write_stereo_arrays_to_wav(self._array, self._title)
 
     def plot_waveform(self):
-        utils.plot_waveform(self._array, ''.join(self._title.split('.')[:-1]))
+        utils.plot_waveform(self._array, self._title)
 
     def plot_fft_spectrum(self, window_size_in_samples):
-        spectrum = utils.get_fft(self._mono_array, window_size_in_samples)
-        utils.plot_fft(spectrum, ''.join(self._title.split('.')[:-1]))
+        spectrum = utils.get_fft(self.mono_array, window_size_in_samples)
+        utils.plot_fft(spectrum, self._title)
 
     def convolve(self, audio_obj):
         conv = array_transforms.convolve(self._array, audio_obj.array)
         rv = Audio()
         rv.set_array(conv)
-        rv.set_title(self.title + 'convolved with' + audio_obj.title)
+        rv.set_title(self._title + ' convolved with ' + audio_obj.title)
         return rv
         
     def pitchshift(self, amt):
         shift = array_transforms.pitchshift(self._array, amt)
         rv = Audio()
         rv.set_array(shift)
-        rv.set_title(self.title + 'pitch shifted %s %'%(amt * 100))
+        rv.set_title(self._title + ' pitch shifted %s % '%(amt * 100))
         return rv
 
     def ringmod(self, freq_hz):
         mod = array_transforms.ringmod(self._array, freq_hz)
         rv = Audio()
         rv.set_array(mod)
-        rv.set_title(self.title + 'ring modulated at %s Hz'%(freq_hz))
+        rv.set_title(self._title + ' ring modulated at %s Hz '%(freq_hz))
         return rv
